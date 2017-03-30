b1b288dd6291c060b881a598cdaa0ea0177e6af3
Andrew Feeney <webspannerdev@gmail.com>
2017-03-31 08:47:31 +1100

    Add constructor and tearDown for TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,15 @@ use Arc\Testing\ArcTestCase;
 
 class TestCase extends ArcTestCase
 {
+    public function __construct()
+    {
+        $this->app = Vendor\PluginName\Plugin::plugin();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
 }
