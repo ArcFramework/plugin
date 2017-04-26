c38c302b3c55c2cace64eeac4f1fa67676c12ffa
Andrew Feeney <webspannerdev@gmail.com>
2017-04-26 16:55:28 +1000

    Implement abstract methods
    These must be implemented here rather than on the abstract class as we need the self:: to refer to the child class and not the abstract class.

--- app/Plugin.php
+++ app/Plugin.php
@@ -6,5 +6,24 @@ use Arc\Application;
 
 class Plugin extends Application
 {
+    /**
+     * Set the shared instance of the application.
+     *
+     * @param  Application|null  $container
+     * @return static
+     */
+    public static function setApplicationInstance(Application $application)
+    {
+        self::$instance = $application;
+    }
 
+    /**
+     * Get the shared instance of the application.
+     *
+     * @return static
+     */
+    public static function app()
+    {
+        return self::$instance;
+    }
 }
