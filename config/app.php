c76ab6c0ba7ce3b0b4265a88478a4c54804bebde
Andrew Feeney <webspannerdev@gmail.com>
2017-04-26 17:17:00 +1000

    Add new config boilerplate

--- config/app.php
+++ config/app.php
@@ -1,5 +1,31 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
 
+    'providers' => [
+
+        /*
+         * Framework Service Providers...
+         */
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+
+        /*
+         * Package Service Providers...
+         */
+
+        /*
+         * Application Service Providers...
+         */
 ];
