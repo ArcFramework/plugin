df56d29b5a01c643acea87aef39812aea90dcc83
Andrew Feeney <webspannerdev@gmail.com>
2017-05-31 00:38:17 +0000

    Apply fixes from StyleCI

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -5,21 +5,19 @@ namespace Vendor\MyPlugin\Providers;
 use Arc\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
-class RouteServiceProvider extends ServiceProvider
+class ViewServiceProvider extends ServiceProvider
 {
     public function boot(Router $router)
     {
         $router->group([
             'namespace' => 'Vendor\MyPlugin\Http\Controllers',
-        ], function() use ($router) {
+        ], function () use ($router) {
 
             // Load browser routes
-            include($this->app->basePath().'/routes/web.php');
-            
+            include $this->app->basePath().'/routes/web.php';
+
             // Load API routes
-            include($this->app->basePath().'/routes/api.php');
-            
+            include $this->app->basePath().'/routes/api.php';
         });
     }
 }
-
