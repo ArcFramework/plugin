af5e16b3dba0793ad2afcbdcefaee7d431ec63c8
Andrew Feeney <webspannerdev@gmail.com>
2017-05-31 10:41:29 +1000

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider. Now create something
+| adequate!
+|
+*/
+
+$routes->get('/some-uri', function () {
+    return view('welcome');
+});
