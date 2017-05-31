062fd7a21bd2c7843730491d177f9b6aeb01482d
Andrew Feeney <webspannerdev@gmail.com>
2017-05-31 10:51:32 +1000

    Create WordpressServiceProvider.php

---
+++ app/Providers/WordpressServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Vendor\MyPlugin\Providers;
+
+use Arc\Admin\AdminMenus;
+use Arc\Hooks\Actions;
+use Arc\Hooks\Filters;
+use Arc\Shortcodes\Shortcodes;
+
+use Illuminate\Support\ServiceProvider;
+
+class WordpressServiceProvider extends ServiceProvider
+{
+    public function boot(
+        Actions $actions,
+        AdminMenus $adminMenus,
+        Filters $filters,
+        Shortcodes $shortcodes
+    )
+    {
+        // Register admin menus
+        include $this->app->basePath().'/wordpress/admin_menus.php';
+        
+        // Register actions
+        include $this->app->basePath().'/wordpress/actions.php';
+        
+        // Register filters
+        include $this->app->basePath().'/wordpress/filters.php';
+        
+        // Register shortcodes
+        include $this->app->basePath().'/wordpress/shortcodes.php';
+    }
+}
