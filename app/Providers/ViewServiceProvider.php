db4fefc0500295c9196ba10c556953db03e16b95
Andrew Feeney <webspannerdev@gmail.com>
2017-05-31 10:38:13 +1000

    Create ViewServiceProvider.php

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vendor\MyPlugin\Providers;
+
+use Arc\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot(Router $router)
+    {
+        $router->group([
+            'namespace' => 'Vendor\MyPlugin\Http\Controllers',
+        ], function() use ($router) {
+
+            // Load browser routes
+            include($this->app->basePath().'/routes/web.php');
+            
+            // Load API routes
+            include($this->app->basePath().'/routes/api.php');
+            
+        });
+    }
+}
+
